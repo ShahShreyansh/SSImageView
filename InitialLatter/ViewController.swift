3e214769c4be49ff0ed84a461bd40f903003048d
Shreyansh Shah <er.snshah12@gmail.com>
2016-08-05 17:16:40 +0530

    Few Changes

--- InitialLatter/ViewController.swift
+++ InitialLatter/ViewController.swift
@@ -12,13 +12,22 @@ import QuartzCore
 class ViewController: UIViewController {
     
     @IBOutlet weak var imgView: SSImageView!
+    @IBOutlet weak var imgUserName: SSImageView!
+    
+    
     @IBOutlet weak var txtName: UITextField!
+    @IBOutlet weak var txtUserName: UITextField!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if txtName.text!.characters.count > 0{
             imgView.setTextToImage(txtName.text!)
+        }
+        
+        if txtUserName.text?.characters.count > 0{
+            imgUserName.setTextToImage(txtUserName.text!)
         }
     }
     
@@ -28,6 +37,27 @@ class ViewController: UIViewController {
         if txtName.text!.characters.count > 0{
             
             imgView.setTextToImage(txtName.text!)
+            
+        }else{
+            
+            let alertController = UIAlertController(title: "", message: "Please enter your full name", preferredStyle: .Alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            self.presentViewController(alertController, animated: true, completion: nil)
+            
+        }
+        
+        
+    }
+    
+    
+    @IBAction func submitBtnPressed(sender: AnyObject) {
+        
+        if txtUserName.text!.characters.count > 0{
+            
+            imgUserName.setTextToImage(txtUserName.text!)
             
         }else{
             
